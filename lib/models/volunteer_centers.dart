831b66b90fbef2961492875ee7e980177f61b2f7
Verkholomova Karina <verholomova_karina@ukr.net>
2022-07-22 14:08:30 +0200

    Added locations

--- lib/models/volunteer_centers.dart
+++ lib/models/volunteer_centers.dart
@@ -1,16 +1,23 @@
 List<Location> LocationList = [
-  Location(longitude: 54.468683, latitude: 17.028140)
+  Location(longitude: 54.468683, latitude: 17.028140, name: "Regionalne Centrum Wolontariatu", address: "aleja Henryka Sienkiewicza 6, 76-200 Słupsk"),
+  Location(longitude: 54.452438, latitude: 17.041785, name: "Pomeranian Academy in Slupsk", address: "Krzysztofa Arciszewskiego, 76-200 Słupsk"),
+  Location(longitude: 54.451206, latitude: 17.023427, name: "Municipal Family Assistance Center", address: "Słoneczna 15D, 76-200 Słupsk"),
+  Location(longitude: 54.458005, latitude: 17.028482, name: "Zespół Szkół Technicznych", address: "Karola Szymanowskiego 5, 76-200 Słupsk")
 ];
 
 class Location{
   double? longitude;
   double? latitude;
+  String? name;
+  String? address;
 
-  Location({this.longitude, this.latitude});
+  Location({this.longitude, this.latitude, this.name, this.address});
 
   Location.fromJson(Map<String, dynamic> json){
     longitude = json['longitude'];
     latitude = json['latitude'];
+    name = json['name'];
+    address = json['address'];
 
   }
 
@@ -18,6 +25,8 @@ class Location{
     final Map<String, dynamic> data = <String, dynamic>{};
     data['longitude']=longitude;
     data['latitude']=latitude;
+    data['name']=name;
+    data['address']=address;
     return data;
   }
 }
